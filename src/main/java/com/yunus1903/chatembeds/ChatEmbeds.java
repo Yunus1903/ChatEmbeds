4c663bae24b8ba7d8df3fe0446aa7636bc40531c
Yunus1903 <yunus1903dj@gmail.com>
2021-02-01 21:18:38 +0100

    Rewritten large parts of code for use of mixin for better compatibility

--- src/main/java/com/yunus1903/chatembeds/ChatEmbeds.java
+++ src/main/java/com/yunus1903/chatembeds/ChatEmbeds.java
@@ -1,16 +1,22 @@
 package com.yunus1903.chatembeds;
 
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixins;
 
 /**
  * @author Yunus1903
  * @since 10/08/2020
  */
 @Mod(ChatEmbeds.MOD_ID)
+@Mod.EventBusSubscriber(value = Dist.CLIENT, modid = ChatEmbeds.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ChatEmbeds
 {
     public static final String MOD_ID = "chatembeds";
@@ -18,6 +24,14 @@ public class ChatEmbeds
 
     public ChatEmbeds()
     {
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () ->
+                Mixins.addConfiguration("chatembeds.mixins.json"));
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ChatEmbedsConfig.CLIENT_SPEC);
+    }
+
+    @SubscribeEvent
+    public static void clientSetup(final FMLClientSetupEvent event)
+    {
+        ChatEmbeds.LOGGER.info("Chat Embeds is being initialized");
     }
 }
